3ccba75693bfba265db60415347eef1fda2e812c
Marco0587 <44323660+Marco0587@users.noreply.github.com>
2020-08-10 18:46:33 +0200

    aggiunto algoritmo consensus e metodo per registrare un nuovo nodo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from flask import Flask, jsonify, request
+import logging
 
 from blockchain import Blockchain
 
@@ -28,10 +29,46 @@ def shutdown():
     shutdown_server()
     return 'Server shutting down...'
 
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    global logger
+    replaced = blockchain.resolve_conflicts( logger )
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
 @app.route('/mine', methods=['GET'])
 def mine():
-    #return "We'll mine a new Block"
-   
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     proof = blockchain.proof_of_work(last_block)
@@ -56,7 +93,8 @@ def mine():
         'previous_hash': block['previous_hash'],
     }
     return jsonify(response), 200
-  
+
+
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     #return "We'll add a new transaction"
@@ -82,5 +120,9 @@ def full_chain():
     }
     return jsonify(response), 200
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger()
+
+    app.run(host='0.0.0.0', port=5000)
--- blockchain.py
+++ blockchain.py
@@ -3,13 +3,27 @@ import numpy as np
 import json
 import hashlib
 from datetime import datetime
+from urllib.parse import urlparse
+import requests
+
 
 class Blockchain():
+
     def __init__(self):
         self.current_transactions = []
         self.chain = []
+
+        self.nodes = set()
+
+        # inizializzo la catena con il primo blocco
         self.add_block(proof = 100, previous_hash = '0'*64)
     
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+    
+
     def add_block(self, proof, previous_hash = None):
         block = {
             'index': len(self.chain) + 1,
@@ -24,6 +38,7 @@ class Blockchain():
         self.chain.append(block)
         return block
     
+
     def add_transaction(self, sender, recipient, amount):
         self.current_transactions.append(
             {
@@ -46,6 +61,7 @@ class Blockchain():
     def last_block(self):
         return self.chain[-1]
 
+
     def proof_of_work(self, last_block):
         last_proof = last_block['proof']
         last_hash = self.hash(last_block)
@@ -55,8 +71,73 @@ class Blockchain():
             proof +=1
         return proof
     
+
     @staticmethod
     def valid_proof(last_proof, proof, last_hash):
-        guess = "{}{}{}".format(last_proof, proof, last_hash).encode()
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash.startswith('00')
+    
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+        last_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+            # Check that the hash of the block is correct
+            last_hash = self.hash(last_block)
+            if block['previous_hash'] != last_hash:
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof'], last_hash):
+                return False
+
+            last_block = block
+            block_index += 1
+
+        return True
+
+
+    def resolve_conflicts(self, logger):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        logger.info("DEBUG - dentro resolve_conflicts")
+
+        neighbours = self.nodes
+        logger.info("DEBUG - neighbours: {}".format(neighbours))
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            logger.info(f"DEBUG - node: {node} --> response: {response}")
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
